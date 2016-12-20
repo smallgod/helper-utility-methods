3c8a62a30c98afb47fdc477e414389dffb0ef189
smallgod <davies.mugume@gmail.com>
2016-12-20 09:12:43 +0300

    December 20th, 2016 09hr11

--- src/main/java/com/library/utilities/HibernateUtils.java
+++ src/main/java/com/library/utilities/HibernateUtils.java
@@ -5,12 +5,14 @@
  */
 package com.library.utilities;
 
-import com.library.datamodel.dsmbridge.TbCustomer;
-import com.library.datamodel.dsmbridge.TbFile;
+import com.library.datamodel.dsm_bridge.TbCustomer;
+import com.library.datamodel.dsm_bridge.TbFile;
+import com.library.datamodel.dsm_bridge.TbFileId;
 import com.library.hibernate.CustomHibernate;
 import com.library.utilities.dsmbridge.IDCreator;
 import static com.library.utilities.GeneralUtils.convertListToSet;
 import java.io.File;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -21,6 +23,36 @@ import java.util.Set;
  */
 public class HibernateUtils {
 
+    private static final LoggerUtil LOG = new LoggerUtil(HibernateUtils.class);
+
+    /**
+     * Generate the FileID To-Do -> Method fetches entire file list for each
+     * call, we need to come up with a better way of doing this.
+     *
+     * @param customHibernate
+     * @return
+     */
+    public static synchronized long generateLongIDOld(CustomHibernate customHibernate) {
+
+        List<TbFileId> fileIdList = customHibernate.fetchOnlyColumn(TbFile.class, "FILE_ID");
+
+        LOG.debug("Records fetched size [TbFile.class] : " + fileIdList.size());
+
+        Set<Long> fileIds = new HashSet<>();
+        for (TbFileId id : fileIdList) {
+            LOG.debug("file id found: " + id.getFileId());
+            fileIds.add(id.getFileId());
+        }
+
+        long fileID;
+
+        do {
+            fileID = IDCreator.GenerateLong();
+        } while (fileIds.contains(fileID));
+
+        return fileID;
+    }
+
     /**
      * Generate the FileID To-Do -> Method fetches entire file list for each
      * call, we need to come up with a better way of doing this.
@@ -30,23 +62,26 @@ public class HibernateUtils {
      */
     public static synchronized long generateLongID(CustomHibernate customHibernate) {
 
-        List<Long> fileIdList = customHibernate.fetchOnlyColumn(TbFile.class, "FILE_ID");
+        List<Long> fileIdList = customHibernate.fetchOnlyColumn(TbFile.class, "id.fileId");
 
-        Set<Long> set = convertListToSet(fileIdList);
+        LOG.debug("Records fetched size [TbFile.class]: " + fileIdList.size());
+
+        Set<Long> fileIds = GeneralUtils.convertListToSet(fileIdList);
 
         long fileID;
 
         do {
             fileID = IDCreator.GenerateLong();
-        } while (set.contains(fileID));
+        } while (fileIds.contains(fileID));
 
         return fileID;
     }
 
     /**
      * Generate a customer ID
+     *
      * @param customHibernate
-     * @return 
+     * @return
      */
     public static synchronized int generateIntegerID(CustomHibernate customHibernate) {
 
